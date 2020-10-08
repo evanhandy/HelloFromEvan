a759e13faea1d8c0e65e1815d0846e6a1fa77b3a
splucena <sumilangplucena@yahoo.com.ph>
2020-10-09 01:34:47 +0800

    added greeting

--- src/handy/HelloFromEvan.java
+++ src/handy/HelloFromEvan.java
@@ -4,6 +4,7 @@ public class HelloFromEvan {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Evan");
+        System.out.println("Hello, Evan, my name is Em");
     }
 
 }
